3e44f94dc839660496fe80004a09c8067657837a
Scott McElroy <scottmcelroy54@gmail.com>
2025-12-02 11:07:33 -0500

    batched the push from expanse, pasted changes here

--- slurm_submit.sh.py
+++ slurm_submit.sh.py
@@ -1,14 +1,26 @@
 #!/bin/bash
+
+#source ~/.bashrc
+
+#module purge
+#module load shared
+#module load cpu/0.17.3b  gcc/10.2.0/npcyll4 openmpi/4.1.1/ygduf2r
+#module load sdsc
+#module load cpu
+#conda avtivate py310
+
 #SBATCH --job-name=NO_diff_test
 #SBATCH -A TG-MED240050
-#SBATCH -t 5:30:00
+#SBATCH -t 3:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=64
 #SBATCH -o simOutput/single.run
 #SBATCH -e simOutput/single.err
 
-#SBATCH --mem=450G
+#SBATCH --mem=240G
 #SBATCH --export=ALL
-#SBATCH --partition=GPU
-source ~/.bashrc
-mpiexec -n 64 nrniv -mpi -python bazh_net.py
+#SBATCH --partition=compute
+
+#cd /home/smcelroy/TC_sleepnet
+
+mpirun -n 64 nrniv -python -mpi bazh_net.py
